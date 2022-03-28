b83ab5ada48d73b29f3791e2fc3da96892490916
strBCL1 <work.artem.popov@gmail.com>
2022-03-28 14:27:43 +0200

    2.2 - Test Classes and Methods

---
+++ JUnitTestProject/src/main/java/com/company/MainClass.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class MainClass {
+    public int sum(int a, int b) {
+        return a + b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Main works");
+    } // 2.3 section
+}
