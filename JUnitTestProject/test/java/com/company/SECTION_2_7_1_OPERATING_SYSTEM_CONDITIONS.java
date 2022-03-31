2c7bdf87e4f04bbd9273871a124e92650bec2204
strBCL1 <work.artem.popov@gmail.com>
2022-03-31 10:19:49 +0200

    2.7.2 - Java Runtime Environment Conditions

--- JUnitTestProject/test/java/com/company/SECTION_2_7_1_OPERATING_SYSTEM_CONDITIONS.java
+++
@@ -1,2 +0,0 @@
-package com.company;public class SECTION_2_7_1_OPERATING_SYSTEM_CONDITIONS {
-}
---
+++ JUnitTestProject/test/java/com/company/SECTION_2_7_2_JAVA_RUNTIME_ENVIRONMENT_CONDITIONS.java
@@ -0,0 +1,2 @@
+package com.company;public class SECTION_2_7_2_JAVA_RUNTIME_ENVIRONMENT_CONDITIONS {
+}
