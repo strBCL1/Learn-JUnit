9296daa8c72d07dfae3116e48a660c49c7081eb6
strBCL1 <work.artem.popov@gmail.com>
2022-04-01 11:07:58 +0200

    2.9.1 - Method Order

---
+++ JUnitTestProject/test/java/com/company/SECTION_2_9_1_METHOD_ORDER.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+final class SECTION_2_9_1_METHOD_ORDER {
+
+    @Test
+    @Order(1)
+    void nullValues() {
+        // perform assertions against null values
+    }
+
+    @Test
+    @Order(2)
+    void emptyValues() {
+        // perform assertions against empty values
+    }
+
+    @Test
+    @Order(3)
+    void validValues() {
+        // perform assertions against valid values
+    }
+}
